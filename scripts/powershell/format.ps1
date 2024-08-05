b0d9bd55e3496b5b020f9e8a95d0c621a01eb4a5
RanukaDinsitha <ranuka@outlook.co.nz>
2024-08-05 18:47:23 +1200

    An update

--- scripts/powershell/format.ps1
+++
@@ -1,3 +0,0 @@
-Write-Output "Executing format Script & In memory of Jesper 🐇🪦"
-Write-Output "-----------------------------------------------------------------------"
-npm run format
--- scripts/powershell/server.ps1
+++
@@ -1,3 +0,0 @@
-Write-Output "Executing localhost Server Script & In memory of Jesper 🐇🪦"
-Write-Output "-----------------------------------------------------------------------"
-npm run server
